1ed5261cec385b02715dc914ade6e0381b59f5a6
Lucaas28 <lucaspicoli.leme@gmail.com>
2025-07-22 13:55:19 -0300

    Ajusta formulario do cabeçalho, ajusta javascript
    Ajustes no formulário do cabeçalho para captação de leads com google
    sheets.
    Criação de um modal de sucesso
    Ajustes no JavaScript para exibição de modal de sucesso, para formatação
    com máscaras nos inputs e para o envio dos dados do formulário direto
    para a planilha do Google

--- js/main.js
+++ js/main.js
@@ -1,150 +1,120 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner(0);
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($('#spinner').length > 0) {
+        $('#spinner').removeClass('show');
+      }
+    }, 1);
+  };
+  spinner(0);
 
+  // Initiate the wowjs
+  new WOW().init();
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
-        } else {
-            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
-        }
-    });
-
-
-    // Hero Header carousel
-    $(".header-carousel").owlCarousel({
-        animateOut: 'slideOutDown',
-        items: 1,
-        autoplay: true,
-        smartSpeed: 500,
-        dots: false,
-        loop: false,
-        nav : true,
-        mouseDrag: false,   // <-- desativa arrasto com o mouse
-        touchDrag: false,   // <-- desativa arrasto com o dedo
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-down"></i>'
-        ],
-    });
-
-
-    // attractions carousel
-    $(".attractions-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 2000,
-        center: false,
-        dots: false,
-        loop: true,
-        margin: 25,
-        nav : true,
-        navText : [
-            '<i class="fa fa-angle-left"></i>',
-            '<i class="fa fa-angle-right"></i>'
-        ],
-        responsiveClass: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:2
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            },
-            1200:{
-                items:4
-            },
-            1400:{
-                items:4
-            }
-        }
-    });
-
-
-    // testimonial carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        center: false,
-        dots: true,
-        loop: true,
-        margin: 25,
-        nav : true,
-        navText : [
-            '<i class="fa fa-angle-left"></i>',
-            '<i class="fa fa-angle-right"></i>'
-        ],
-        responsiveClass: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:1
-            },
-            992:{
-                items:1
-            },
-            1200:{
-                items:1
-            }
-        }
-    });
-
-
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 5,
-        time: 2000
-    });
-
-
-   // Back to top button
-   $(window).scroll(function () {
-    if ($(this).scrollTop() > 600) {
-        $('.back-to-top-float').fadeIn('slow').css('display', 'flex');
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.sticky-top').addClass('shadow-sm').css('top', '0px');
     } else {
-        $('.back-to-top-float').fadeOut('slow');
+      $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
     }
-    });
-    $('.back-to-top-float').click(function () {
-        $('html, body').animate({scrollTop: 0}, 300, 'easeInOutExpo');
-        return false;
-    });
+  });
 
-    //Faq
-    document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle').forEach((faqItem) => {
-        faqItem.addEventListener('click', () => {
-        faqItem.parentNode.classList.toggle('faq-active');
-        });
+  // Hero Header carousel
+  $(".header-carousel").owlCarousel({
+    animateOut: 'slideOutDown',
+    items: 1,
+    autoplay: true,
+    smartSpeed: 500,
+    dots: false,
+    loop: false,
+    nav : true,
+    mouseDrag: false,   // <-- desativa arrasto com o mouse
+    touchDrag: false,   // <-- desativa arrasto com o dedo
+    navText : [
+      '<i class="bi bi-arrow-left"></i>',
+      '<i class="bi bi-arrow-down"></i>'
+    ],
+  });
+
+  // attractions carousel
+  $(".attractions-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 2000,
+    center: false,
+    dots: false,
+    loop: true,
+    margin: 25,
+    nav : true,
+    navText : [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'
+    ],
+    responsiveClass: true,
+    responsive: {
+      0: { items:1 },
+      576: { items:2 },
+      768: { items:2 },
+      992: { items:3 },
+      1200: { items:4 },
+      1400: { items:4 }
+    }
+  });
+
+  // testimonial carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    center: false,
+    dots: true,
+    loop: true,
+    margin: 25,
+    nav : true,
+    navText : [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'
+    ],
+    responsiveClass: true,
+    responsive: {
+      0: { items:1 },
+      576: { items:1 },
+      768: { items:1 },
+      992: { items:1 },
+      1200: { items:1 }
+    }
+  });
+
+  // Facts counter
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 5,
+    time: 2000
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 600) {
+      $('.back-to-top-float').fadeIn('slow').css('display', 'flex');
+    } else {
+      $('.back-to-top-float').fadeOut('slow');
+    }
+  });
+  $('.back-to-top-float').click(function () {
+    $('html, body').animate({scrollTop: 0}, 300, 'easeInOutExpo');
+    return false;
+  });
+
+  // Faq toggle
+  document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle').forEach((faqItem) => {
+    faqItem.addEventListener('click', () => {
+      faqItem.parentNode.classList.toggle('faq-active');
     });
+  });
 
-    // Data e Relógio em tempo real
-    function atualizarDataHora() {
-
+  // Data e Relógio em tempo real
+  function atualizarDataHora() {
     const agora = new Date();
     const utc = agora.getTime() + (agora.getTimezoneOffset() * 60000);
     const brasilia = new Date(utc - (3 * 60 * 60000)); // UTC-3
@@ -167,5 +137,45 @@
   setInterval(atualizarDataHora, 1000);
   atualizarDataHora();
 
-})(jQuery);
+  // Aqui vamos garantir que o DOM está pronto para manipular formulários e máscara
+  $(document).ready(function () {
 
+    // Form submit com AJAX, evitando recarregar página
+    $('#formLead').submit(function(e) {
+        e.preventDefault();
+
+        // Remove "R$" do campo de valor antes de enviar
+        var valorOriginal = $('#valorDesejado').val();
+        var valorFormatado = valorOriginal.replace('R$', '').trim();
+        $('#valorDesejado').val(valorFormatado);
+
+        var dados = $(this).serialize();
+
+        // Restaura o valor com "R$" no input (opcional, para a experiência do usuário)
+        $('#valorDesejado').val(valorOriginal);
+
+        $.post($(this).attr('action'), dados)
+            .done(function(resposta) {
+            $('#formLead')[0].reset();
+            var modal = new bootstrap.Modal(document.getElementById('modalSucesso'));
+            modal.show();
+
+            setTimeout(function () {
+                modal.hide();
+            }, 4000); // Fecha o modal após 4 segundos
+        });
+    });
+
+
+    // Máscara do telefone
+    $('#phone').mask('(00) 00000-0000');
+
+    $(document).ready(function() {
+        // Máscara para o campo de valor com R$
+        $('#valorDesejado').mask('R$ 000.000.000,00', {reverse: true});
+    });
+
+
+  });
+
+})(jQuery);
